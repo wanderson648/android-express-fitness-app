54b80c84af679d91e3a031f4837292fb3397b965
unknown <testpedroca@gmail.com>
2022-12-20 15:00:10 -0300

    Intents e Activities

--- app/src/main/java/com/example/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.fitnessapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var btnImc: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnImc = findViewById(R.id.btn_imc)
+
+
+        btnImc.setOnClickListener {
+            startActivity(Intent(
+                this,
+                FormImcActivity::class.java
+            ))
+        }
+
+
     }
 }
