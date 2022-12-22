426bdf7577e54c6fcb23f7ee74fef052f7ac4fa6
unknown <testpedroca@gmail.com>
2022-12-22 17:27:33 -0300

    Regras de nogócios para cálculo do imc

--- app/src/main/java/com/example/fitnessapp/FormImcActivity.kt
+++ app/src/main/java/com/example/fitnessapp/FormImcActivity.kt
@@ -1,9 +1,11 @@
 package com.example.fitnessapp
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 
 class FormImcActivity : AppCompatActivity() {
@@ -31,8 +33,39 @@ class FormImcActivity : AppCompatActivity() {
                 ).show()
                 return@setOnClickListener
             }
+
+            val height = editHeight.text.toString().toInt()
+            val weight = editWeight.text.toString().toInt()
+
+            val result = calculateImc(height, weight)
+            val imcResposeId = imcResponse(result)
+            Log.i("test", "imc: $result")
+
+            Toast.makeText(
+                this,
+                imcResposeId,
+                Toast.LENGTH_LONG
+            ).show()
         }
 
+    }
+
+    @StringRes
+    private fun imcResponse(imc: Double): Int {
+        return when {
+            imc < 15.0 -> R.string.imc_severely_low_weight
+            imc < 16.0 -> R.string.imc_very_low_weight
+            imc < 18.5 -> R.string.imc_low_weight
+            imc < 25.0 -> R.string.normal
+            imc < 30.0 -> R.string.imc_high_weight
+            imc < 35.0 -> R.string.imc_so_high_weight
+            imc < 40.0 -> R.string.imc_severely_high_weight
+            else -> R.string.imc_extreme_weight
+        }
+    }
+    
+    private fun calculateImc(height: Int, weight: Int): Double {
+        return weight / ((height / 100.0) * (height / 100.0))
     }
 
     private fun validate() : Boolean {
